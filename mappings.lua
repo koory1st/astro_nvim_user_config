c668117c38da76f80036328355f47b35b1606e19
levy <koory1st@manjaro.com>
2023-01-22 20:40:47 +0800

    feat: new init

--- mappings.lua
+++ mappings.lua
@@ -27,6 +27,12 @@ return {
     -- hop
     ["f"] = { "<cmd>HopChar1AC<cr>", desc = "hop ac" },
     ["F"] = { "<cmd>HopChar1BC<cr>", desc = "hop bc" },
+    ["<leader>lr"] = {function() require("telescope.builtin").lsp_references() end, desc="References"},
+    ["<leader>lR"] = {function() vim.lsp.buf.rename() end, desc="Rename"},
+    ["<leader>lD"] = {function() require("telescope.builtin").lsp_definitions() end, desc="Definitions"},
+    ["<F9>"] = {"<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>", desc="Toggle Breakpoint"},
+    ["<leader>DB"] = {"<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>", desc="Clear All Breakpoint"},
+    ["<leader>DC"] = {"<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<cr>", desc="Set Conditional Breakpoint"},
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,8 @@
 return {
-
   -- You can disable default plugins as follows:
-  ["goolord/alpha-nvim"] = { disable = true },
   ['phaazon/hop.nvim'] = {
     branch = 'v2', -- optional but strongly recommended
+    as = "hop",
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
       require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
@@ -17,26 +16,42 @@ return {
       require('pretty-fold').setup()
     end
   },
-  ["init"] = {
-    -- Extended file type support
-    { "sheerun/vim-polyglot" },
-
-    -- Rust support
-    {
-      "simrat39/rust-tools.nvim",
-      after = { "mason-lspconfig.nvim" },
-      -- Is configured via the server_registration_override installed below!
-      config = function()
-        require("rust-tools").setup {
-          server = astronvim.lsp.server_settings "rust_analyzer",
-          tools = {
-            inlay_hints = {
-              parameter_hints_prefix = "  ",
-              other_hints_prefix = "  ",
-            },
-          },
-        }
-      end,
-    },
+  ['simrat39/rust-tools.nvim'] = {
+    as = 'rust-tools',
+    config = function()
+      require("rust-tools").setup()
+    end,
   },
+  -- ['nacro90/numb.nvim'] = {
+  --   as = 'numb',
+  --   config = function()
+  --     require('numb').setup{
+  --       show_numbers = true, -- Enable 'number' for the window while peeking
+  --       show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+  --       hide_relativenumbers = true, -- Enable turning off 'relativenumber' for the window while peeking
+  --       number_only = false, -- Peek only when the command is only a number instead of when it starts with a number
+  --       centered_peeking = true, -- Peeked line will be centered relative to window
+  --     }
+  --   end,
+  -- },
+  ["folke/neodev.nvim"] = {
+    as = 'neodev',
+    config = function()
+      require("neodev").setup()
+    end,
+  },
+  ["klen/nvim-test"] = {
+    as = 'nvim-test',
+    config = function()
+      require('nvim-test').setup()
+    end,
+  },
+  ['Weissle/persistent-breakpoints.nvim'] = {
+    as = 'persistent-breakpoints',
+    config = function()
+      require('persistent-breakpoints').setup{
+        load_breakpoints_event = { "BufReadPost" }
+      }
+    end,
+  }
 }
